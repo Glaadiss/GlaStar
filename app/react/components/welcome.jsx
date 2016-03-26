92f30fc13277daa203b6311c4de02ae5acf89a2f
maricha <maricha17@gazeta.pl>
2016-03-26 22:35:23 +0100

    Welcome prawie git

--- app/react/components/welcome.jsx
+++ app/react/components/welcome.jsx
@@ -3,34 +3,89 @@ import React from 'react'
 class Welcome extends React.Component {
 	render() {
 		return(
+		<div>
+			<nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		        <div className="container">
+		            <div className="navbar-header">
+		                <a className="navbar-brand" href="#">
+		                    <img src="http://placehold.it/150x50&text=Logo" alt="gfg"></img>
+		                </a>
+		            </div>
+		            <div className="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+		                <ul className="nav navbar-nav">
+		                    <li>
+		                        <a href="users/sign_up">Sign up</a>
+		                    </li>
+		                    <li>
+		                        <a href="users/sign_in">Sign in</a>
+		                    </li>
+		                </ul>
+		            </div>
+		        </div>
+		    </nav>
+	    
+	    <div className="container">
 
-			<div id="banner">
-				<div className="banner_content">
-					<h1>GlarStar</h1>
-					<p>Twoje blogi cos tam cos tam</p>
-	    			<button>Click me</button>
-				</div>
+	        
+	        <div className="row">
+	            <div className="col-md-12 bgpicture">
+	            	<div className="text-center napis">
+						<h1>GlarStar Company</h1>
+		                <p>Chcesz zrobić sobie bloga? No to dawej synu!</p>
+		                <a className="btn btn-primary btn-lg" href="users/sign_up">Sign up!</a>
+	                </div>
 
-				<div id="testimonial">
-					<div className="wrapper">
-						<p className="quote">Najlepsza aplikacja blogowa w internetach</p>
-						<p className="name">Marcin Starmach</p>
-					</div>
-				</div>
+	            </div>
+	            
+	        </div>
+	        
 
-				<div id="callouts">
-					<div className="callout_inner">
-						<div className="wrapper"> 
-							<div className="callout"> 
-								<h2> Notes </h2>
-								<p>SDFsfgdfgdgdfg</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+	        
 
-			);
+	        <div className="row">
+	            <div className="col-lg-12">
+	                <div className="well text-center">
+	                    Najlepsze miejsce do blogowania i dzielenia się z innymi swoją pasją!
+	                </div>
+	            </div>
+	            
+	        </div>
+	        
+
+	        
+	        <div className="row">
+	            <div className="col-md-4">
+	                <h2>Cos 1</h2>
+	                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
+	                <a className="btn btn-default" href="#">More Info</a>
+	            </div>
+	            
+	            <div className="col-md-4">
+	                <h2>Cos 2</h2>
+	                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
+	                <a className="btn btn-default" href="#">More Info</a>
+	            </div>
+	            
+	            <div className="col-md-4">
+	                <h2>cos 3</h2>
+	                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
+	                <a className="btn btn-default" href="#">More Info</a>
+	            </div>
+	            
+	        </div>
+	      
+
+		        <footer>
+		            <div className="row">
+		                <div className="col-lg-12">
+		                    <p>Copyright &copy; GlarStar Company 2016 </p>
+		                </div>
+		            </div>
+		        </footer>
+		    </div>
+		   
+	    </div>
+	  );
 	}
 }
 
