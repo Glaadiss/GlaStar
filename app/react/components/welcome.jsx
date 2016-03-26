3b605b8c67d25bdfbd315b550eea6feac769ccda
maricha <maricha17@gazeta.pl>
2016-03-26 13:28:38 +0100

    Dodane welcome

---
+++ app/react/components/welcome.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Welcome extends React.Component {
+	render() {
+		return(
+
+			<div>
+				<p>To jest witanie heheh xD</p>
+			</div>
+
+			);
+	}
+}
+
+export default Welcome;
