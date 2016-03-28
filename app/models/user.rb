6334a61a3077aaf9d365887c73cbd5233215eef6
Glaadiss <bartekgladys@gmail.com>
2016-03-28 12:09:16 +0200

    images should works

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ActiveRecord::Base
   has_many :pages   
   has_many :conversations, :foreign_key => :sender_id 
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>", face: "50x50!" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>", face: "50x50!" }, default_url: "http://placehold.it/50x50"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
